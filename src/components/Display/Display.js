6acd1ab46bc57cac6c20d11123d80b71298f562f
cmutagorama <cmutagorama@gmail.com>
2019-07-30 17:50:08 +0200

    First commit

---
+++ src/components/Display/Display.js
@@ -0,0 +1,13 @@
+import React from "react";
+import propTypes from "prop-types";
+import "./Display.css";
+
+const Display = ({ displayValue }) => (
+  <div className="display-container">
+    <p className="display-value">{displayValue}</p>
+  </div>
+);
+
+Display.propTypes = { displayValue: propTypes.string.isRequired };
+
+export default Display;
